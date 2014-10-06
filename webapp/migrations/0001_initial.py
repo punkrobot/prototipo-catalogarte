c61fb4dd39a71e44ff0384320917ca160869eb97
Juan Rodriguez <juan.pj@gmail.com>
2014-10-06 13:29:15 -0500

    Modelos Museo y Catalogo actualizados

--- webapp/migrations/0001_initial.py
+++ webapp/migrations/0001_initial.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import autoslug.fields
+import jsonfield.fields
 from django.conf import settings
 
 
@@ -13,21 +15,54 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Catalogo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('fecha_inicial', models.DateTimeField()),
+                ('fecha_final', models.DateTimeField(null=True, blank=True)),
+                ('titulo', models.CharField(max_length=100)),
+                ('autor', models.CharField(max_length=100, blank=True)),
+                ('creditos', models.TextField(blank=True)),
+                ('informacion', models.TextField(blank=True)),
+                ('actividades', models.TextField(blank=True)),
+                ('website', models.URLField(max_length=100, blank=True)),
+                ('slug', autoslug.fields.AutoSlugField(editable=False)),
+                ('portada', models.ImageField(upload_to=b'')),
+                ('num_paginas', models.IntegerField(null=True, blank=True)),
+                ('fecha_publicacion', models.DateTimeField(null=True, blank=True)),
+                ('fecha_modificacion', models.DateTimeField(null=True, blank=True)),
+                ('publicado', models.NullBooleanField()),
+                ('contenido', jsonfield.fields.JSONField(blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Museo',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('nombre', models.CharField(max_length=100)),
                 ('direccion', models.TextField()),
+                ('detalles', models.TextField(blank=True)),
+                ('website', models.URLField(max_length=100, blank=True)),
+                ('twitter', models.URLField(max_length=100, blank=True)),
+                ('facebook', models.URLField(max_length=100, blank=True)),
+                ('youtube', models.URLField(max_length=100, blank=True)),
+                ('instagram', models.URLField(max_length=100, blank=True)),
+                ('slug', autoslug.fields.AutoSlugField(editable=False)),
                 ('logotipo', models.ImageField(upload_to=b'')),
-                ('telefono', models.CharField(max_length=100)),
-                ('horarios', models.CharField(max_length=100)),
-                ('website', models.URLField(max_length=100)),
-                ('twitter', models.URLField(max_length=100)),
-                ('facebook', models.URLField(max_length=100)),
+                ('portada', models.ImageField(upload_to=b'', blank=True)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='catalogo',
+            name='museo',
+            field=models.ForeignKey(to='webapp.Museo'),
+            preserve_default=True,
         ),
     ]
--- webapp/models.py
+++ webapp/models.py
@@ -1,6 +1,11 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
 from django.contrib.auth.models import User
+from django.db import models
+from django.forms import MultipleChoiceField
+
+from autoslug import AutoSlugField
+from jsonfield import JSONField
 
 class Museo(models.Model):
     user = models.OneToOneField(User)
@@ -8,11 +13,46 @@ class Museo(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.TextField()
     #email = models.EmailField(blank=True)
-    telefono = models.CharField(max_length=100, blank=True)
-    horarios = models.CharField(max_length=100, blank=True)
+    detalles = models.TextField(blank=True)
 
-    website = models.URLField(max_length=100)
+    website = models.URLField(max_length=100, blank=True)
     twitter = models.URLField(max_length=100, blank=True)
     facebook = models.URLField(max_length=100, blank=True)
+    youtube = models.URLField(max_length=100, blank=True)
+    instagram = models.URLField(max_length=100, blank=True)
 
+    slug = AutoSlugField(populate_from='nombre')
     logotipo = models.ImageField()
+    portada = models.ImageField(blank=True)
+
+class Catalogo(models.Model):
+    museo = models.ForeignKey(Museo)
+
+    fecha_inicial = models.DateTimeField()
+    fecha_final = models.DateTimeField(null=True, blank=True)
+
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=100, blank=True)
+    creditos = models.TextField(blank=True)
+    informacion = models.TextField(blank=True)
+    actividades = models.TextField(blank=True)
+    website = models.URLField(max_length=100, blank=True)
+
+    slug = AutoSlugField(populate_from='titulo')
+    portada = models.ImageField()
+
+    CATEGORIAS_CHOICES = (
+        ('AP', 'Artes plásticas'),
+        ('AV', 'Artes visuales'),
+        ('F', 'Fotografía'),
+        ('C', 'Ciencia'),
+        ('I', 'Infantil'),
+    )
+    categorias = MultipleChoiceField(choices=CATEGORIAS_CHOICES)
+
+    num_paginas = models.IntegerField(null=True, blank=True)
+    fecha_publicacion = models.DateTimeField(null=True, blank=True)
+    fecha_modificacion = models.DateTimeField(null=True, blank=True)
+    publicado = models.NullBooleanField()
+
+    contenido = JSONField(blank=True)
